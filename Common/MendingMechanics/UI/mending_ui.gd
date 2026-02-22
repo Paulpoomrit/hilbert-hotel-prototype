a381f4b3889b6aba2a5dcfbfde32806bad64a1a2
Paulpoomrit <paulpoomrit@gmail.com>
2026-02-21 18:37:18 -0800

    Removed linked list stuff
    Thanks Kevin T0T

---
+++ Common/MendingMechanics/UI/mending_ui.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+@onready var mending_area: GridContainer = $HBoxContainer/MendingArea
+@onready var inventory: GridContainer = $HBoxContainer/Inventory
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for block in mending_area.columns:
+		print(block)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- Common/MendingMechanics/block.gd
+++ Common/MendingMechanics/block.gd
@@ -1,12 +1,13 @@
 class_name Block
 
 
-extends Node
+extends TextureButton
 
 
 @export var _block_type: String = "null"
-var _previous_block: Block = null
-var _next_block: Block = null
+@export var _hover_tint: Color = Color(0.41, 0.41, 0.41, 1.0)
+
+
 var _is_enable: bool = false
 
 
@@ -18,36 +19,6 @@ func set_block_type(new_type: String) -> void:
 	_block_type = new_type
 
 
-func get_previous_block() -> Block:
-	return _previous_block
-
-
-func get_next_block() -> Block:
-	return _next_block
-
-
-func set_previous_block(new_prev_block: Block) -> void:
-	_previous_block = new_prev_block
-
-
-func set_next_block(new_next_block: Block) -> void:
-	_next_block = new_next_block
-
-
-func is_first_node() -> bool:
-	if _previous_block == null:
-		return true
-	else:
-		return false
-
-
-func is_last_node() -> bool:
-	if _next_block == null:
-		return true
-	else:
-		return false
-
-
 func enable_block() -> void:
 	_is_enable = true
 	# handle block enable effects
@@ -56,3 +27,11 @@ func enable_block() -> void:
 func disable_block() -> void:
 	_is_enable = false
 	# handle block disable effects
+
+
+func _on_mouse_entered() -> void:
+	self.modulate = _hover_tint
+
+
+func _on_mouse_exited() -> void:
+	self.modulate = Color(1,1,1,1)
