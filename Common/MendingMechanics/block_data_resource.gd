e31f0a547c1bf213eb55ca807828e619dea50994
Paulpoomrit <paulpoomrit@gmail.com>
2026-02-23 17:14:17 -0800

    Added block sprites

--- Common/MendingMechanics/block_data_resource.gd
+++ Common/MendingMechanics/block_data_resource.gd
@@ -4,6 +4,6 @@ class_name BlockData
 extends Resource
 
 
-@export var block_type: String = "null"
+@export var block_type: String = "Null"
 @export var block_texture: Texture2D
 @export var hover_tint: Color = Color(0.41, 0.41, 0.41, 1.0)
