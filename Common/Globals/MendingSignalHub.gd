bde236c47e876a521a0a6643dd9234b2f5bc00fb
Paulpoomrit <paulpoomrit@gmail.com>
2026-03-04 19:49:43 -0800

    Removed unused UI var

--- Common/Globals/MendingSignalHub.gd
+++ Common/Globals/MendingSignalHub.gd
@@ -7,8 +7,6 @@ signal on_change_gravity_type(new_val: Variant, negated: bool, target: Object)
 signal on_change_speed_type(new_val: Variant, negated: bool, target: Object)
 signal on_change_time_type(new_val: Variant, negated: bool, target: Object)
 
-var is_dragging = false
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
