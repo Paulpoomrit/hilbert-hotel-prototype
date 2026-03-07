107391cf8eb24208a8f967aa7135a6cc1c7d046d
KKang <kang78030@gmail.com>
2026-03-07 22:35:27 +0800

    Implemented mendable gravity

--- Common/MendableComponents/mendable_gravity.gd
+++ Common/MendableComponents/mendable_gravity.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-var gravity_function = "linear"
+var real = true
 var gravity_direction = Vector2(0, 1.0)
 var gravity_multiplier = 1.0
 
@@ -15,9 +15,12 @@ func get_gravity_direction():
 
 
 func modify_gravity(grav: Vector2):
-	return (grav
-		- (ProjectSettings.get_setting("physics/2d/default_gravity_vector") * ProjectSettings.get_setting("physics/2d/default_gravity"))
-		+ (gravity_direction * ProjectSettings.get_setting("physics/2d/default_gravity") * gravity_multiplier))
+	return (
+			(grav
+			- (ProjectSettings.get_setting("physics/2d/default_gravity_vector") * ProjectSettings.get_setting("physics/2d/default_gravity"))
+			+ (gravity_direction * ProjectSettings.get_setting("physics/2d/default_gravity") * gravity_multiplier))
+		if real
+		else Vector2(0.0, 0.0))
 
 
 func _on_change_gravity_type(new_val, negated : bool = false, target = null):
@@ -25,5 +28,9 @@ func _on_change_gravity_type(new_val, negated : bool = false, target = null):
 		var parent = get_parent()
 		if not parent or not is_instance_of(parent, target):
 			return
-		print('a')
-	print('b')
+	if typeof(new_val) == TYPE_INT:
+		gravity_multiplier = new_val
+		if negated:
+			gravity_multiplier *= -1
+	elif new_val == "Real":
+		real = !negated
