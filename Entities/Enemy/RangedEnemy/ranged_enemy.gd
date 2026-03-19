32c7ee67aa00c4fa38dfffecf32e5dacc3f54c8b
KKang <kang78030@gmail.com>
2026-03-19 04:32:34 -0700

    Added a simple ranged enemy.

---
+++ Entities/Enemy/RangedEnemy/ranged_enemy.gd
@@ -0,0 +1,58 @@
+extends Enemy
+
+
+@export var attack_rate : float = 2
+@export var projectile_speed : float = 10.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+var _attack_timer : float = 0.0
+var _player_ref : Node2D = null
+var _projectile_scene : PackedScene = preload("res://Entities/Enemy/RangedEnemy/RangedEnemyProjectile.tscn")
+
+
+func _ready() -> void:
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	## Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+#
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if _player_ref:
+		_attack_timer += delta
+		if _attack_timer >= attack_rate:
+			# Throws projectile at player's current location
+			var projectile = _projectile_scene.instantiate()
+			projectile.position = position
+			projectile.linear_velocity = Vector2(20.0, 0.0)
+			projectile.point_to(_player_ref.global_position)
+			get_parent().add_child(projectile)
+			# Reset attack timer
+			_attack_timer = fmod(_attack_timer, attack_rate)
+	else: 
+		_attack_timer = 0.0
+	
+	move_and_slide()
+
+
+func _on_throwable_range_body_entered(body: Node2D) -> void:
+	if body is Player:
+		_player_ref = body
+
+
+func _on_throwable_range_body_exited(body: Node2D) -> void:
+	if body is Player:
+		_player_ref = null
